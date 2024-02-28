044779040b8d87aeb5f6745095ae8fe9e19168c4
Stino89 <ftr1989@gmail.com>
2024-02-27 20:30:00 -0800

    4

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Description: This file is used to export the snake-names package.
 const snakeNames = require('snake-names')
+const snakeNames = require('snake-names')
+
+let namePicked = snakeNames.random()
+
+console.log('You should name this snake:', namePicked)
